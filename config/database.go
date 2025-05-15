337e1ea854a9271ddc0256d2d71936d137b50125
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-05-15 15:53:14 +0700

    feat: database configuration

---
+++ config/database.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := "root:@tcp(127.0.0.1:3306)/golang_backend?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database Connected")
+	DB = db
+}
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"github.com/farinchan/golang_backend/config"
 	"github.com/farinchan/golang_backend/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+
+	//init Database
+	config.ConnectDB()
+
 	app := fiber.New()
 
 	//init Route
 	routes.ApiRoute(app)
+	
 
 	app.Listen(":3000")
 }
